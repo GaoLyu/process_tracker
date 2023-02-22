63e0251ab195da7a60c5abbf40d7b16563b0de2f
Gaoyueyang Lyu <gaoyueyang.lyu@gmail.com>
2023-02-22 09:35:11 -0500

    Update process_tracker.c

--- process_tracker.c
+++ process_tracker.c
@@ -35,53 +35,51 @@ bool isnumber(char string[]){
 }
 
 
-void getFD(char pid[]){
-   char location[20];
+int getone(char pid[], int i, int f){
+   char location[100];
    char path[130];
-   char filename[150];
+   char filename[1024];
    strcpy(location, "/proc/");
    strcat(location,pid);
    strcat(location,"/fd");
    DIR* dirp1=opendir(location);
    if (dirp1 == NULL) {
-      perror("opendir");
-      if (errno == EACCES) {
-         fprintf(stderr, "Skipping this pid\n");
-         return;
-      }
-      exit(1);
+      return i;
    }
    struct dirent* dp;
    while ((dp = readdir(dirp1)) != NULL) {
       if(isnumber(dp->d_name)){
-         printf("pid: %s   ",pid);
-         printf("fd: %s  ",dp->d_name);
-         printf("inode: %lu   ",dp->d_ino);
-         //char path[130];
          strcpy(path,location);
          strcat(path,"/");
-         strcat(path,dp->d_name);
-         // char filename[150];
-         // struct stat st;
-         // stat(path,&st);
-         // struct stat fst;
-         // fstat(atoi(dp->d_name),&fst);
-
-         // printf("inode: from stat()%lu, from fstat() %lu\n",st.st_ino,
-         //    fst.st_ino);
-         // printf("uid: from stat() %u, from fstat() %u\n",st.st_uid,
-         //  fst.st_uid);         
-         readlink(path,filename,150);
-         printf("filename: %s\n", filename);
+         strcat(path,dp->d_name);     
+         readlink(path,filename,1024);
+         if(f==0){
+            //per-process
+            printf("%d\t%s\t%s\n",i,pid,dp->d_name);
+         }
+         else if(f==1){
+            //systemWide
+            printf("%d\t%s\t%s\t%s\n",i,pid,dp->d_name,filename);
+         }
+         else if(f==2){
+            //Vnodes
+            printf("%d\t%lu\t%s\t%s\n",i,dp->d_ino);
+         }
+         else if(f==3){
+            //composite
+            printf("%d\t%s\t%s\t%lu\t%s\n",i,pid,dp->d_name,dp->d_ino,filename);
+         }
          memset(path, 0, 130);
-         memset(filename, 0, 150);
+         memset(filename, 0, 1024);
+         i++;
       }
   }
   closedir(dirp1);
-
+  return i;
 }
 
-void getprocesses(){
+void composite(){
+   
    DIR* dirp=opendir("/proc");
    if (dirp == NULL) {
       perror("opendir");
@@ -89,32 +87,33 @@ void getprocesses(){
    }
    struct dirent* dp;
    uid_t uid=getuid();
-   char path[120];
+   char path[130];
    int i=0;
    struct stat st;
    while ((dp = readdir(dirp)) != NULL) {
-      printf("testtest %d\n",i);
+      //printf("testtest %d\n",i);
       if(isnumber(dp->d_name) && dp->d_type == DT_DIR){
          strcpy(path,"/proc/");
          strcat(path,dp->d_name);
          stat(path,&st);
          if(st.st_uid==uid){
-            getFD(dp->d_name);
+            i=getone(dp->d_name,i,3);
          }
-         memset(path, 0, 120);
+         memset(path, 0, 130);
          
-         printf(">>>>i");
-         i++;
+         //printf(">>>>i");
+         //i++;
       }
    }
    closedir(dirp);
-
 }
 
 
+void perprocess(){
 
+}
 
-void get_command(int argc, char **argv, struct option long_options[], int* pid){
+void get_command(int argc, char **argv, struct option long_options[], char pid[]){
    int c;
    int option_index;
    while((c=getopt_long(argc,argv,"",long_options,&option_index))!=-1){
@@ -129,7 +128,7 @@ void get_command(int argc, char **argv, struct option long_options[], int* pid){
    }
    while(optind<argc){
       if(isnumber(argv[optind])){
-         *pid=atoi(argv[optind]);
+         strcpy(pid, argv[optind]);
          break;
       }
       optind++;
@@ -142,7 +141,7 @@ int main(int argc, char **argv){
    int vnodes_flag=0;
    int composite_flag=0;
    int threshold=-1;
-   int pid=-1;
+   char pid[10];
    struct option long_options[]={
       {"per-process",0,&process_flag,1},
       {"systemWide",0,&system_flag,1},
@@ -151,8 +150,11 @@ int main(int argc, char **argv){
       {"threshold",1,&threshold,-2}, //check if threshold is called and assigned a value
       {0,0,0,0}
    };
-   get_command(argc,argv,long_options,&pid);
-   getprocesses();
-   
+
+   get_command(argc,argv,long_options,pid);
+
+   //getprocesses();
+   getone(pid,0);
+   composite();
    return 0;
 }
