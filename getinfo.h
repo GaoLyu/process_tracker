7c4627b939e6bf00d432b25d48e2b943c93b4e7e
Gaoyueyang Lyu <gaoyueyang.lyu@gmail.com>
2023-03-05 09:18:22 -0500

    Update getinfo.h

--- getinfo.h
+++ getinfo.h
@@ -28,10 +28,20 @@ struct process{
    int num;
 };
 
+
+//Returns pointer to a struct file with information fd, filename, inode
 struct file* createfile(char fd[], char filename[], int inode);
+
+//Returns pointer to a struct process with information pid, file, i which is number of fd
 struct process* createprocess(char pid[], struct file* file, int i);
+
+//Returns true if string is only made up of numbers. Returns false otherwise
 bool isnumber(char string[]);
+
+//Returns the number of fd inside pid, and store all fd in f, a linked list of struct file
 int getfd(char pid[], struct file **f);
+
+//Finds all current running process and store them in a linked list of struct process
 void getallpid(struct process** p);
 
 #endif
